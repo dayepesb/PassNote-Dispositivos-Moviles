6e8480c72e19bb7ddd4869a5fad429adc65df104
jacastrorug <juliancastroruge@hotmail.com>
2017-08-18 00:34:34 -0500

    Proyecto Maven
    Se crea el proyecto Maven con la integración para la infraestructura con
    Spring Boot

---
+++ PassNote/src/main/java/passnote/poli/edu/co/PassNote/PassNoteApplication.java
@@ -0,0 +1,12 @@
+package passnote.poli.edu.co.PassNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassNoteApplication.class, args);
+	}
+}
---
+++ PassNote/src/test/java/passnote/poli/edu/co/PassNote/PassNoteApplicationTests.java
@@ -0,0 +1,16 @@
+package passnote.poli.edu.co.PassNote;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PassNoteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
