f8e1ac4e0eb49de181e1bf445fa61adaa4551e3b
David yepes <lion17-07@hotmail.com>
2017-09-01 04:07:44 -0500

    Actividad SingIn
    Se añade la actividad SingIn sins ericio solo lo visual

--- PassNote_Apk/app/src/main/java/co/edu/poli/passnote/passnote/Login.java
+++ PassNote_Apk/app/src/main/java/co/edu/poli/passnote/passnote/Login.java
@@ -1,13 +1,20 @@
 package co.edu.poli.passnote.passnote;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_login);
         setContentView(R.layout.activity_login);
+    }
+    public void register(View v){
+        Intent intent = new Intent(this,SinginActivity.class);
+        startActivity(intent);
     }
 }
